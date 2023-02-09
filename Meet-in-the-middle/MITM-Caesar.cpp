b1ab7c53b3c5f41827854e9dacb292f7a2b41fe3
HLRichardson <101138530+HLRichardson-Git@users.noreply.github.com>
2023-02-09 15:02:55 -0600

    Create MITM-Caesar.cpp

---
+++ Meet-in-the-middle/MITM-Caesar.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string caesarCipher(string plainText, int key);
+
+class Table {
+        string cipherText;
+        int key_l;
+    public:
+        void putEntry(string plainText, int key);
+        void getEntry();
+};
+
+int main()
+{
+    
+    Table table[26];
+
+    string knownPlainText = "Hello World";
+    int key1 = 4; int key2 = 13;
+
+    string cipherText = caesarCipher(knownPlainText, key1);
+    cout << cipherText << endl;
+
+    cipherText = caesarCipher(cipherText, key2);
+    cout << cipherText << endl;
+
+    for(int i = 0; i < 26; i++)
+    {
+        table[i].putEntry(knownPlainText, i);
+    }
+    
+    for(int i = 0; i < 26; i++)
+    {   
+        cout << i << ": ";
+        table[i].getEntry();
+    }
+
+    return 0;
+}
+
+string caesarCipher(string plainText, int key)
+{
+
+    string result = "";
+ 
+    for (int i = 0; i < plainText.length(); i++) {
+
+        if (plainText[i] == 32)
+            result += char(plainText[i]);
+        else if (isupper(plainText[i]))
+            result += char(int(plainText[i] + key - 65) % 26 + 65);
+        else
+            result += char(int(plainText[i] + key - 97) % 26 + 97);
+    }
+
+    return result;
+
+}
+
+void Table::putEntry(string plainText, int key)
+{
+
+    cipherText = caesarCipher(plainText, key);
+    key_l = key;
+
+}
+
+void Table::getEntry()
+{
+
+    cout << cipherText << endl;
+
+}
